ca636c03d689deab657930b7df2c64cc836b1c5a
Samantha Ponce <samanthaponce5@gmail.com>
2020-03-08 21:35:58 +0000

    Automatically backed up by Learn

--- learning-comments.rb
+++ learning-comments.rb
@@ -1,1 +1,15 @@
-%w[this is also a test].join(" ").capitalize 
+class Application
+ 
+  @@items = ["Apples","Carrots","Pears"]
+ 
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+ 
+    @@items.each do |item|
+      resp.write "#{item}\n"
+    end
+ 
+    resp.finish
+  end
+end
